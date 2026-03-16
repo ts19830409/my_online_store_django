39f188d1fc8c179a22a6cbac1c2cd63ae7059079
Tsoy Aleksey <i2705@rambler.ru>
2026-03-17 00:11:25 +1000

    домашнее задание завершено

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def catalog(request):
+	"""Отображение страницы'Каталог'"""
+	return render(request, 'catalog.html')
+
+def contacts(request):
+	"""Отображение страницы 'Контакты'"""
+	if request.method == "POST":
+		name = request.POST.get("name")
+		phone = request.POST.get("phone")
+		message = request.POST.get("message")
+		
+		print(f"Получено сообщение от {name}, телефон: {phone}, сообщение: '{message}'")
+		return HttpResponse(f"Спасибо за обратную связь, {name}. Сообщение получено!")
+	
+	return render(request, 'contacts.html')
